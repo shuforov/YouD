080495bfc75c14112cac17eee4818fb3de9c18fe
shuforov <shuforov@gmail.com>
2016-04-29 21:33:45 +0300

    print the image on form

--- Ui_YouD/Ui_YouD_ui.py
+++ Ui_YouD/Ui_YouD_ui.py
@@ -1,4 +1,6 @@
 import sys
+import pafy
+
 from PyQt4 import QtGui,QtCore
 
 class Icon(QtGui.QWidget):
@@ -13,15 +15,29 @@ class Icon(QtGui.QWidget):
         self.tb.clicked.connect(self.take_inf)
 
         self.le = QtGui.QLineEdit(self)
-        self.le.setGeometry(11,40,200,20)
+        self.le.setGeometry(11,40,350,20)
+
+        self.ql = QtGui.QLabel('Image', self)
+        self.ql.setGeometry(13,55,200,200)
+        self.ql.setPixmap(QtGui.QPixmap('image.jpg'))
 
 
 
     def take_inf(self):
         text = self.le.text()
         print text
+        url = pafy.new(text)
+        print '------------------'
+        image_url = 'Image', url.bigthumb
+        print url.bigthumb
+        print 'Title:', url.title
+        print 'Duration:', url.duration
+        print '------------------'
 
 app = QtGui.QApplication(sys.argv)
 icon = Icon()
 icon.show()
 sys.exit(app.exec_())
+
+
+#https://www.youtube.com/watch?v=9ZetitIsaSM
