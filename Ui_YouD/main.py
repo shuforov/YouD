64c5a990ef4136b37ccfb8b7ea4a0c1426fbcb26
shuforov <shuforov@gmail.com>
2016-05-21 18:39:49 +0300

    add to main chose_dir window

--- Ui_YouD/main.py
+++ Ui_YouD/main.py
@@ -2,6 +2,7 @@ import sys
 import pafy
 from PyQt4 import QtGui, QtCore
 from Ui_YouD import Ui_Ui_YouD
+from Ui_YouD_path import Ui_path_form
 
 
 class YouD(QtGui.QWidget, Ui_Ui_YouD):
@@ -14,7 +15,8 @@ class YouD(QtGui.QWidget, Ui_Ui_YouD):
         self.connect(self.UrlButton, QtCore.SIGNAL("clicked()"),self.takeandadd)
         self.comboBox_format_type.activated[str].connect(self.chose_type)
         self.connect(self.DownloadButton, QtCore.SIGNAL("clicked()"),self.download_url)
-
+        self.connect(self.Path_button, QtCore.SIGNAL("clicked()"),self.YouD_path_f)
+        self.window2 = None
 
     # take url and add items to check box and list
     def takeandadd(self):
@@ -140,6 +142,47 @@ class YouD(QtGui.QWidget, Ui_Ui_YouD):
             dict_list_form_all_type[text_d].download(filepath=path_file, quiet=True, callback=progress)
         # list_form_all_type[0].download(filepath='')
 
+    def YouD_path_f(self):
+        if self.window2 is None:
+            self.window2 = YouD_path(self)
+        self.window2.show()
+
+
+class YouD_path(QtGui.QWidget, Ui_path_form):
+    def __init__(self,yyy):
+        QtGui.QWidget.__init__(self)
+        Ui_path_form.__init__(self)
+        self.setupUi(self)
+        # create model
+        model = QtGui.QFileSystemModel()
+        model.setRootPath(QtCore.QDir.currentPath())
+
+        self.pathRoot = QtCore.QDir.rootPath()
+        self.model = QtGui.QFileSystemModel(self)
+        self.model.setRootPath(self.pathRoot)
+
+        self.indexRoot = self.model.index(self.model.rootPath())
+
+        self.treeView_dir.setModel(self.model)
+        self.treeView_dir.setRootIndex(self.indexRoot)
+        self.treeView_dir.clicked.connect(self.chose_dir)
+
+        # close button
+        self.connect(self.cancel_button, QtCore.SIGNAL("clicked()"), self.set_path)
+        # chose button
+        self.connect(self.chose_dir_path, QtCore.SIGNAL("clicked()"), self.set_path)
+
+    def chose_dir(self, index):
+        indexItem = self.model.index(index.row(), 0, index.parent())
+
+        fileName = self.model.fileName(indexItem)
+        filePath = self.model.filePath(indexItem)
+
+        self.label_name.setText(fileName)
+        self.label_path.setText(filePath)
+
+    def set_path(self):
+        self.close()
 
 if __name__ == '__main__':
     app = QtGui.QApplication(sys.argv)
